b20c24c9c2102d40ff4a0325d941cc762db9c35a
Pascal Harprecht <p.harprecht@gmail.com>
2022-02-09 18:04:06 +0000

    Testing slash commands into modals

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
+use dotenv::dotenv;
+use serenity::{
+    async_trait,
+    client::{Context, EventHandler},
+    model::{
+        channel::Message,
+        id::GuildId,
+        interactions::{
+            message_component::{ActionRowComponent, ButtonStyle, InputTextStyle},
+            Interaction, InteractionResponseType,
+        },
+        prelude::Ready,
+    },
+    Client,
+};
 use std::env;
 use tracing_subscriber;
-use dotenv::dotenv;
-use serenity::{Client, async_trait, client::{EventHandler, Context}, model::{channel::Message, interactions::{Interaction, message_component::{ButtonStyle, InputTextStyle}, InteractionResponseType}}};
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| {
+                command
+                    .name("modal")
+                    .description("Trigger a modal from slash commands")
+            })
+        })
+        .await;
+        println!(
+            "I now have the following guild slash commands: {:#?}",
+            commands
+        );
+    }
+
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content != "modal" {
             return;
         }
 
-        msg
-            .channel_id
+        msg.channel_id
             .send_message(&ctx, |m| {
                 m.content("Press the button for modal =D");
                 m.components(|c| {
@@ -37,35 +73,106 @@ impl EventHandler for Handler {
                 mci.create_interaction_response(&ctx, |r| {
                     r.kind(InteractionResponseType::Modal);
                     r.interaction_response_data(|d| {
-                        d.custom_id("modal");
-                        d.title("Modals POG");
+                        d.custom_id("modal_comp_interaction");
+                        d.title("Modals from Component Interaction");
                         d.components(|c| {
                             c.create_action_row(|ar| {
                                 ar.create_input_text(|it| {
-                                    it
-                                        .style(InputTextStyle::Short)
+                                    it.style(InputTextStyle::Short)
                                         .custom_id("input_text_one")
+                                        .required(true)
+                                        .value("This is pre filled for you")
                                         .label("Short test")
                                 })
                             });
                             c.create_action_row(|ar| {
                                 ar.create_input_text(|it| {
-                                    it
-                                        .style(InputTextStyle::Paragraph)
+                                    it.style(InputTextStyle::Paragraph)
                                         .custom_id("input_text_two")
+                                        .required(false)
                                         .label("Loooooooooong text")
                                 })
                             })
                         })
                     })
                 })
-                    .await
-                    .unwrap();
-            },
+                .await
+                .unwrap();
+            }
+            Interaction::ApplicationCommand(aci) => {
+                aci.create_interaction_response(&ctx, |r| {
+                    r.kind(InteractionResponseType::Modal);
+                    r.interaction_response_data(|d| {
+                        d.custom_id("modal_app_cmd");
+                        d.title("Modals from Slash Commands");
+                        d.components(|c| {
+                            c.create_action_row(|ar| {
+                                ar.create_input_text(|it| {
+                                    it.style(InputTextStyle::Short)
+                                        .custom_id("input_text_one")
+                                        .required(false)
+                                        .label("Short test")
+                                })
+                            });
+                            c.create_action_row(|ar| {
+                                ar.create_input_text(|it| {
+                                    it.style(InputTextStyle::Paragraph)
+                                        .custom_id("input_text_two")
+                                        .required(true)
+                                        .min_length(200)
+                                        .placeholder("Please write an essay")
+                                        .label("Loooooooooong text")
+                                })
+                            })
+                        })
+                    })
+                })
+                .await
+                .unwrap();
+            }
             Interaction::ModalSubmit(mci) => {
-                dbg!(mci);
-            },
-            _ => ()
+                dbg!(&mci);
+                let short_text = match mci
+                    .data
+                    .components
+                    .get(0)
+                    .unwrap()
+                    .components
+                    .get(0)
+                    .unwrap()
+                {
+                    ActionRowComponent::InputText(it) => it,
+                    _ => return,
+                };
+                let long_text = match mci
+                    .data
+                    .components
+                    .get(1)
+                    .unwrap()
+                    .components
+                    .get(0)
+                    .unwrap()
+                {
+                    ActionRowComponent::InputText(it) => it,
+                    _ => return,
+                };
+                mci.create_interaction_response(ctx, |r| {
+                    if mci.data.custom_id == "modal_app_cmd" {
+                        r.kind(InteractionResponseType::ChannelMessageWithSource);
+                    } else {
+                        r.kind(InteractionResponseType::UpdateMessage);
+                    }
+                    r.interaction_response_data(|d| {
+                        d.create_embed(|e| {
+                            e.title(&short_text.value);
+                            e.field(&long_text.custom_id, &long_text.value, false)
+                        })
+                    })
+                })
+                .await
+                .unwrap();
+            }
+            _ => (),
         }
     }
 }
